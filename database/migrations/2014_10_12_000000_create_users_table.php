85e09f7b003b82117472cf19baf9f5e660f7ba5e
ArvinAjifTechology <arvinajif3@gmail.com>
2023-05-09 06:34:41 +0700

    Update Tbbel Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('no_id')->unique()->nullable();
             $table->string('email')->unique();
+            $table->enum('level', ['admin', 'teacher', 'student'])->nullable();
+            $table->enum('gender', ['laki-laki', 'perempuan'])->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
