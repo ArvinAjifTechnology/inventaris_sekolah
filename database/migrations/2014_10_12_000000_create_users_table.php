9899aa2fc0c0466f903faa3cc52dc3cb82ccd445
ArvinAjifTechology <arvinajif3@gmail.com>
2023-05-09 22:01:55 +0700

    Update Tabel User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('user_id');
+            $table->string('username')->unique()->nullable();
+            $table->string('user_code')->unique()->nullable();
+            $table->string('email')->unique();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
-            $table->string('no_id')->unique()->nullable();
-            $table->string('email')->unique();
             $table->enum('level', ['admin', 'teacher', 'student'])->nullable();
             $table->enum('gender', ['laki-laki', 'perempuan'])->nullable();
             $table->timestamp('email_verified_at')->nullable();
