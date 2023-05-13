d9de008669bd2d36b4f9648c808ffe6c1e387cc1
ArvinAjifTechology <arvinajif3@gmail.com>
2023-05-13 11:13:50 +0700

    Update Model Borrow

--- app/Models/Borrow.php
+++ app/Models/Borrow.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Borrow extends Model
 {
     use HasFactory;
+
+    public static function getAll()
+    {
+        return DB::select('select * from borrows');
+    }
 }
