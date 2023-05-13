dffc199547e86e6386eb362e106b3a82d369bfa4
ArvinAjifTechology <arvinajif3@gmail.com>
2023-05-13 11:35:57 +0700

    Update Model Borrow

--- app/Models/Borrow.php
+++ app/Models/Borrow.php
@@ -14,4 +14,34 @@ class Borrow extends Model
     {
         return DB::select('select * from borrows');
     }
+
+    public static function findId($id)
+    {
+        DB::select('select * from items where id = ?', [$id]);
+    }
+
+    public static function insert($request)
+    {
+        DB::insert('INSERT INTO items (item_code,item_name,room_id,description, `condition`, amount) VALUES (?, ?, ?, ?, ?,?)', [
+            $request->input('item_code'),
+            $request->input('item_name'),
+            $request->input('room_id'),
+            $request->input('description'),
+            $request->input('condition'),
+            $request->input('amount'),
+        ]);
+    }
+
+    public static function edit($request)
+    {
+        DB::insert('UPDATE items SET item_code= ? ,item_name= ? ,room_id= ? ,description= ? , `condition`= ? , amount= ? WHERE id = ?', [
+            $request->input('item_code'),
+            $request->input('item_name'),
+            $request->input('room_id'),
+            $request->input('description'),
+            $request->input('condition'),
+            $request->input('amount'),
+            $request->input('id')
+        ]);
+    }
 }
