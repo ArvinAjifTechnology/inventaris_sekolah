6966e922bdd09d12193505c26a58cabd2787d1e6
ArvinAjifTechology <arvinajif3@gmail.com>
2023-05-13 10:09:02 +0700

    Update User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,19 +12,19 @@ use Illuminate\Support\Facades\Validator;
 class UserController extends Controller
 {
     /**
-    * Display a listing of the resource.
-    */
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $users=   DB::select('SELECT * FROM users ORDER BY created_at DESC');
+        $users = User::getAll();
         // $users = User::all();
         return view('users.index', compact('users'));
     }
 
     /**
-    * Show the form for creating a new resource.
-    */
-    public function create($id=NULL)
+     * Show the form for creating a new resource.
+     */
+    public function create($id = NULL)
     {
         $user = DB::select('select * from users where id = ?', [1]);
         $user = Collection::make($user);
@@ -32,8 +32,8 @@ class UserController extends Controller
     }
 
     /**
-    * Store a newly created resource in storage.
-    */
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -48,94 +48,73 @@ class UserController extends Controller
 
         if ($validator->fails()) {
             return redirect('/users/create')
-            ->withErrors($validator)
-            ->withInput();
+                ->withErrors($validator)
+                ->withInput();
         }
         $fullName = $request->first_name . $request->last_name;
 
-        DB::insert('INSERT INTO users (name,username,user_code,email,first_name, last_name,level, gender, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [
-            $request->input($fullName),
-            $request->input('username'),
-            $request->input('user_code'),
-            $request->input('email'),
-            $request->input('first_name'),
-            $request->input('last_name'),
-            $request->input('level'),
-            $request->input('gender'),
-            bcrypt($request->input('email'))
-        ]);
+        User::insert($request);
 
-        return redirect()->back()->withErrors($validator)->withInput();
+        return redirect()->back()->withErrors($validator)->with('status', 'Selamat Data Berhasil Di Tambahkan')->withInput();
     }
 
     /**
-    * Display the specified resource.
-    */
+     * Display the specified resource.
+     */
     public function show(string $id)
     {
         //
     }
 
     /**
-    * Show the form for editing the specified resource.
-    */
+     * Show the form for editing the specified resource.
+     */
     public function edit(string $id)
     {
-        $user = DB::select('select * from users');
+        $user = DB::select('select * from users  where username = ?', [$id]);
         // $user = Collection::make($user);
         // $user = collect($user)->map(function($item) {
-            //     return (object) $item;
-            // });
-            // $user = User::find($id);
-            // dd($user);
-            return view('users.edit', compact('user', 'id'));
-        }
+        //     return (object) $item;
+        // });
+        // $user = User::find($id);
+        // dd($user);
+        return view('users.edit', compact('user', 'id'));
+    }
 
-        /**
-        * Update the specified resource in storage.
-        */
-        public function update(Request $request, string $username)
-        {
-            $user = DB::select('select * from users where username = ?', [$username]);
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $username)
+    {
+        $user = DB::select('select * from users where username = ?', [$username]);
 
-            $validator = Validator::make($request->all(), [
-                'first_name' => ['required', 'string'],
-                'last_name' => ['required', 'string'],
-                'user_code' => ['required', 'string', Rule::unique('users')->ignore($user[0]->id)],
-                'username' => ['required', 'string', Rule::unique('users')->ignore($user[0]->id)],
-                'email' => ['required', 'email', Rule::unique('users')->ignore($user[0]->id)],
-                'level' => ['required', 'string'],
-                'gender' => ['required', 'string'],
-            ]);
+        $validator = Validator::make($request->all(), [
+            'first_name' => ['required', 'string'],
+            'last_name' => ['required', 'string'],
+            'user_code' => ['required', 'string', Rule::unique('users')->ignore($user[0]->id)],
+            'username' => ['required', 'string', Rule::unique('users')->ignore($user[0]->id)],
+            'email' => ['required', 'email', Rule::unique('users')->ignore($user[0]->id)],
+            'level' => ['required', 'string'],
+            'gender' => ['required', 'string'],
+        ]);
 
-            if ($validator->fails()) {
-                return redirect('/users/create')
+        if ($validator->fails()) {
+            return redirect('/users/create')
                 ->withErrors($validator)
                 ->withInput();
-            }
-            $fullName = $request->input('first_name') . $request->input('last_name');
-            DB::update('UPDATE users SET name = ?, username = ?, user_code = ?, email = ?, first_name = ?, last_name = ?, level = ?, gender = ?, password = ? WHERE username = ?', [
-                $fullName,
-                $request->input('username'),
-                $request->input('user_code'),
-                $request->input('email'),
-                $request->input('first_name'),
-                $request->input('last_name'),
-                $request->input('level'),
-                $request->input('gender'),
-                bcrypt($request->input('email')),
-                $username
-            ]);
-
-            return redirect('/users')->withErrors($validator)->withSuccess('Selamat Data Berhasil Di Update')->withInput();
         }
+        $fullName = $request->input('first_name') . $request->input('last_name');
+        User::edit($fullName, $request, $username);
 
-        /**
-        * Remove the specified resource from storage.
-        */
-        public function destroy(string $username)
-        {
-            DB::delete('DELETE FROM users WHERE username = ?', [$username]);
-            return redirect('/users')->with('success', 'Data berhasil Di Hapus');
-        }
+        return redirect('/users')->withErrors($validator)->withSuccess('Selamat Data Berhasil Di Update')->with('status', 'Selamat Data Berhasil Di Update')->withInput();
     }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $username)
+    {
+        User::destroy($username);
+        return redirect('/users')->with('status', 'Data berhasil Di Hapus');
+    }
+}
