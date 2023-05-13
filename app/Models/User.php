d09ccf52ad2fabb4ffc671a206512457729de6e3
ArvinAjifTechology <arvinajif3@gmail.com>
2023-05-13 10:09:48 +0700

    Update User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -43,6 +44,51 @@ class User extends Authenticatable
     ];
 
     /**
+     * Fungsi CRUD
+     */
+
+    public static function getAll()
+    {
+        return DB::select('SELECT * FROM users ORDER BY created_at DESC');
+    }
+
+    public static function insert($request)
+    {
+        DB::insert('INSERT INTO users (name,username,user_code,email,first_name, last_name,level, gender, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+            $request->input('name'),
+            $request->input('username'),
+            $request->input('user_code'),
+            $request->input('email'),
+            $request->input('first_name'),
+            $request->input('last_name'),
+            $request->input('level'),
+            $request->input('gender'),
+            bcrypt($request->input('email'))
+        ]);
+    }
+
+    public static function edit($fullName, $request, $username)
+    {
+        DB::update('UPDATE users SET name = ?, username = ?, user_code = ?, email = ?, first_name = ?, last_name = ?, level = ?, gender = ?, password = ? WHERE username = ?', [
+            $fullName,
+            $request->input('username'),
+            $request->input('user_code'),
+            $request->input('email'),
+            $request->input('first_name'),
+            $request->input('last_name'),
+            $request->input('level'),
+            $request->input('gender'),
+            bcrypt($request->input('email')),
+            $username
+        ]);
+    }
+
+    public static function destroy($username)
+    {
+        DB::delete('DELETE FROM users WHERE username = ?', [$username]);
+    }
+
+    /**
      * Fungsi Ini Digunakan Untuk Mengambil FullName User
      */
 
@@ -55,6 +101,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Room::class);
     }
-
-
 }
